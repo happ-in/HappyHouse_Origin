873e1c8d81d486f3f0edf8498856c9e597bf47d3
hyebinppark <gpqls304@naver.com>
2021-05-18 23:43:55 +0900

    add: init

--- front/happyhouse/src/App.vue
+++ front/happyhouse/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-link to="/main">Home</router-link> | <router-link to="/apt">아파트정보</router-link> |
+    <router-link to="/">Home</router-link> | <router-link to="/apt">아파트정보</router-link> |
     <router-link to="/qna">QnA</router-link>
     <router-view></router-view>
   </div>
