4d27fccb964beb91f82b7e4b2c9661f37b1c26ae
hyebinppark <gpqls304@naver.com>
2021-05-21 14:06:30 +0900

    add: page size

--- back/happyhouse/src/main/java/com/ssafy/happyhouse/utils/StringUtil.java
+++ back/happyhouse/src/main/java/com/ssafy/happyhouse/utils/StringUtil.java
@@ -3,4 +3,6 @@ package com.ssafy.happyhouse.utils;
 public class StringUtil {
     public final static String SUCCESS = "success";
     public final static String FAIL = "fail";
+
+    public static final int SIZE = 10;
 }
